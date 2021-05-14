9d1cd5a973c0b9713db8c7b95f9da75729e8f2e2
OttoNyman <7716116@gmail.com>
2021-05-14 17:23:38 +0300

    zalivaem vsio

---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+const Profile = () => {
+    return <div className='content'>
+        <div>
+          <img src="https://luxfon.com/images/201203/luxfon.com_4224.jpg" alt="" />
+        </div>
+        <div>
+          ava+description
+        </div>
+        <div>
+          My posts
+           <div>
+            New post
+           </div>
+          <div>
+            <div>Post1</div>
+            <div>Post2</div>
+            <div>Post3</div>
+          </div>
+        </div>
+      </div>
+}
+export default Profile;
