40b2ef738a4d7dab94d641bc2c4c0f7fdb3098fb
OttoNyman <7716116@gmail.com>
2021-05-16 14:46:12 +0300

    added css modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
+import s from "./Header.module.css";
+
 const Header = () => {
-    return (
-         <header className='header'>
-        <img src="https://static4.depositphotos.com/1020822/350/v/600/depositphotos_3501921-stock-illustration-photography-eye-logo.jpg" alt="" />
-      </header>
-    )
-}
+  return (
+    <header className={s.header}>
+      <img
+        src="https://static4.depositphotos.com/1020822/350/v/600/depositphotos_3501921-stock-illustration-photography-eye-logo.jpg"
+        alt=""
+      />
+    </header>
+  );
+};
 
 export default Header;
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,22 +1,25 @@
+import s from "./Profile.module.css";
+
 const Profile = () => {
-    return <div className='content'>
-        <div>
-          <img src="https://luxfon.com/images/201203/luxfon.com_4224.jpg" alt="" />
-        </div>
-        <div>
-          ava+description
-        </div>
-        <div>
-          My posts
-           <div>
-            New post
-           </div>
-          <div>
-            <div>Post1</div>
-            <div>Post2</div>
-            <div>Post3</div>
-          </div>
+  return (
+    <div className={s.content}>
+      <div>
+        <img
+          src="https://luxfon.com/images/201203/luxfon.com_4224.jpg"
+          alt=""
+        />
+      </div>
+      <div>ava+description</div>
+      <div>
+        My posts
+        <div>New post</div>
+        <div className={s.posts}>
+          <div className={s.item}>Post1</div>
+          <div className={s.item}>Post2</div>
+          <div className={s.item}>Post3</div>
         </div>
       </div>
-}
+    </div>
+  );
+};
 export default Profile;
