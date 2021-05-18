1c217c2939a9d3b21d2a740091efbd57359f9a86
OttoNyman <7716116@gmail.com>
2021-05-18 12:46:19 +0300

    Dialogs page added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
-import MyPosts from './components/Profile/MyPosts/MyPosts';
+import Dialogs from "./components/Dialogs/Dialogs";
+// import MyPosts from './components/Profile/MyPosts/MyPosts';
+import s from './'
 
 const App = () => {
-  return (
-    <div className='app-wrapper '>
-      <Header />
-      <Navbar />
-      <Profile />
-    </div>
-  );
+    return (
+        <div className='app-wrapper '>
+            <Header/>
+            <Navbar/>
+            <div className='app-wrapper-content'>
+                <Dialogs/>
+                {/*<Profile />*/}
+            </div>
+        </div>
+    );
 }
 
 export default App;
